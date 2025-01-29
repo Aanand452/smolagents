40ec6bc1f2b48e782d5a84b1bf8b744b1178f477
Anand452 <anandkumars016@gmail.com>
2025-01-29 12:23:24 -0600

    added the smolagents

---
+++ agents.py
@@ -0,0 +1,31 @@
+import os 
+import smolagents
+from smolagents import CodeAgent, HfApiModel
+from dotenv import load_dotenv
+import openai 
+from openai import OpenAI
+
+
+load_dotenv()
+
+api_key = os.getenv("OPEN_API_KEY")
+
+client = OpenAI(api_key=api_key)
+
+
+hf_api_key = os.getenv("HF_TOKEN")
+
+
+
+
+model = HfApiModel()
+
+agent =CodeAgent(tools=[],model=model)
+
+print(agent.run("what is the  temperature my location"))
+
+
+
+
+
+
